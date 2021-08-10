fbdece2c886edffc4e3d3f119b22711ff817d82b
huligabuliga <jonastclausen@gmail.com>
2021-08-10 16:22:07 -0500

    lists

---
+++ Learn The Basics/Lists/lists.py
@@ -0,0 +1,17 @@
+mylist = [] #creates list
+mylist.append(1) #appends (Addss to list)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+
+
+#####accesing an index which does not exist generates an exception 
+# mylist = [1,2,3]
+# print(mylist[10])
+#####
